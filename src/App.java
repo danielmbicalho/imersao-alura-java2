d7fc364917e1e9427da2954bbf8c1f1f0b6f7268
Daniel Magalhães Bicalho <danielmbicalho@gmail.com>
2023-03-30 22:10:52 -0300

    Alterações para impressão colorida, adição de emojis

--- src/App.java
+++ src/App.java
@@ -19,19 +19,19 @@
 
             // Configurar os emojis
             // Unicode emoji https://unicode.org/emoji/charts/full-emoji-list.html#1f600
-            // Conversor de código https://www.fileformat.info/info/unicode/char/search.htm
+            // Conversor de código 
             String starEmoji = "\u2B50";
             // Usar emoji para as notas 
             // 9 a 10
-            String grinningFaceEmoji = "\uD83D\uDE00";
+            // String grinningFaceEmoji = "\uD83D\uDE00";
             // 7 a 8
-            String smileFaceEmoji = "\uD83D\uDE42";
+            // String smileFaceEmoji = "\uD83D\uDE42";
             //5 a 6
-            String neutralFaceEmoji = "\uD83D\uDE10";
+            // String neutralFaceEmoji = "\uD83D\uDE10";
             // 3 a 5
-            String disapointedFaceEmoji = "\uD83D\uDE1E";
+            // String disapointedFaceEmoji = "\uD83D\uDE1E";
             // 1 a 3
-            String sleepyFaceEmoji = "\uD83D\uDE2A";
+            // String sleepyFaceEmoji = "\uD83D\uDE2A";
 
             Properties imdbProps = new Properties();
             imdbProps.load(new FileInputStream(imdbConfigPath));
@@ -46,13 +46,12 @@
             endpoints.add("Top250TVs");
             endpoints.add("MostPopularMovies");
             endpoints.add("MostPopularTVs");
-            // String topTVsURL = "https://imdb-api.com/en/API/Top250TVs/" + imdbKey;
-
+            
             var parser = new JsonParser();
             
-            // for (String endpoint : endpoints) {
-                // String address = baseURL + endpoint + "/" + imdbKey;
-                String address = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+             for (String endpoint : endpoints) {
+                String address = baseURL + endpoint + "/" + imdbKey;
+                //String address = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
                 URI topMovieAddress = URI.create(address);
                 var client = HttpClient.newHttpClient();
                 var request = HttpRequest.newBuilder(topMovieAddress).GET().build();
@@ -63,34 +62,35 @@
 
                 // exibir e manipular os dados 
                 for (Map<String,String> filme : listaDeFilmes) {
-                    System.out.println(filme.get("title"));
-                    System.out.println(filme.get("image"));
-                    System.out.println(filme.get("imDbRating"));
+                    System.out.printf("\u001b[1m \u001b[43m Movie Title: %s \u001b[m %n", filme.get("title"));
+                    System.out.printf("\u001b[1m \u001b[42m Movie Poster: %s  \u001b[m %n", filme.get("image"));
+                    System.out.printf("\u001b[35;1m \u001b[45m imDb Rating: %s \u001b[m %n", filme.get("imDbRating"));
                     
                     float rateInt = Float.parseFloat(filme.get("imDbRating"));
 
                     for (int rate=0; rate < rateInt; rate++){
-                        System.out.print(starEmoji);
+                        System.out.printf("\u001b[44m%s \u001b[m", starEmoji);
                     }
-                    System.out.print("\n User experience: ");
+                    
+                    String experience = "";
                     
                     if (rateInt >= 8){
-                        System.out.print(grinningFaceEmoji);
+                        experience = "\uD83D\uDE00";
                     }else if (rateInt < 8 && rateInt >= 6 ){
-                        System.out.print(smileFaceEmoji);   
+                        experience = "\uD83D\uDE42";   
                     }else if (rateInt < 6 && rateInt >= 5 ){
-                        System.out.print(neutralFaceEmoji);   
-                    }else if (rateInt < 5 && rateInt >= 4 ){
-                        System.out.print(neutralFaceEmoji);
-                    }else if (rateInt < 4 && rateInt >= 3 ){
-                        System.out.print(disapointedFaceEmoji);
-                    }
-                    else{
-                        System.out.print(sleepyFaceEmoji);   
+                        experience = "\uD83D\uDE10";
+                    }else if (rateInt < 5 && rateInt >= 3 ){
+                        experience = "\uD83D\uDE1E";
+                    }else {
+                        experience = "\uD83D\uDE2A";
                     }
 
-                    System.out.println();
-            // }
+
+                    System.out.printf("%n \u001b[47m User experience: %s \u001b[m %n ", experience);
+
+                    System.out.println("\n");
+            }
             
         }
             
